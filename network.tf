84407698d275f46eda0c73bf705cb01121c508ab
zaynapsabry <zeinabsabry340@gmail.com>
2024-05-06 20:09:21 +0300

    Pushing the whole project

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source = "E:/ITI - 9 months/Course32_Terraform/Lec2/terraform/lab2/network-module"
+  source = "E:/ITI - 9 months/Course32_Terraform/Lec2/terraform/network-module"
   region = var.region
   vpc_cidr = var.vpc_cidr
   public_subnet_cidr = var.public_subnet_cidr
